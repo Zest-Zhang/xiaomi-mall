99a9b33aa9d5ebb8334bf85bd24ba02ddc308fbf
Zest-Zhang <2521946696zzt@gmail.com>
2023-07-05 16:05:53 +0800

    feat(接口数据): 完成orderRouter.js和collectRouter.js的api（xiaomi-mall后端部分已完成）

--- koa/src/routers/index.js
+++ koa/src/routers/index.js
@@ -8,10 +8,14 @@ const usersRouter = require('./router/userRouter');
 const resourcesRouter = require('./router/resourcesRouter');
 const productRouter = require('./router/productRouter');
 const shoppingCartRouter = require('./router/shoppingCartRouter');
+const orderRouter = require('./router/orderRouter');
+const collectRouter = require('./router/collectRouter');
 
 Routers.use(usersRouter.routes());
 Routers.use(resourcesRouter.routes())
 Routers.use(productRouter.routes())
 Routers.use(shoppingCartRouter.routes());
+Routers.use(orderRouter.routes())
+Routers.use(collectRouter.routes());
 
 module.exports = Routers;
